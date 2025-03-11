b89f8bf000606305efa6092d5e350f769ba2a572
Zihang <wenzihang0506@gmail.com>
2025-03-11 10:14:34 -0500

    update eval max_pop

--- RL_agent/simulation_test.py
+++ RL_agent/simulation_test.py
@@ -27,27 +27,31 @@ if MAIN:
     antibiotic_value = float(sys.argv[3])
     nutrient_value = float(sys.argv[4])
     rep = int(sys.argv[5])
+    results_dir = sys.argv[6]
+
+    num_decisions = 300
 
     cell_config = CellConfig()
     env_config = EnvConfig(k_n0_constant = nutrient_value,
                            warm_up = 37, delay_embed_len = 10,
-                           b_actions = [0, antibiotic_value])
+                           b_actions = [0, antibiotic_value],
+                           max_pop = np.inf)
 
     env = ConstantNutrientEnv(env_config, cell_config)
 
-    folder_name="results/a%.2f_n%.2f_value_check/%s_%d/"%(antibiotic_value, nutrient_value, initialize_app, half_period)
+    folder_name=f"{results_dir}/a{antibiotic_value:.2f}_n{nutrient_value:.2f}_value_check/{initialize_app}_{half_period}/"
     os.makedirs(folder_name, exist_ok=True)
 
     if initialize_app == "low":
-        decisions = ([0] * half_period + [1] * half_period) * (200 // 2 // half_period + 1)
+        decisions = ([0] * half_period + [1] * half_period) * (num_decisions // 2 // half_period + 1)
     elif initialize_app == "high":
-        decisions = ([1] * half_period + [0] * half_period) * (200 // 2 // half_period + 1)
+        decisions = ([1] * half_period + [0] * half_period) * (num_decisions // 2 // half_period + 1)
     elif initialize_app == "constant":
-        decisions = [1] * 200
+        decisions = [1] * num_decisions
     elif initialize_app == "constant_low":
-        decisions = [0] * 200
+        decisions = [0] * num_decisions
 
-    decisions = decisions[:200]
+    decisions = decisions[:num_decisions]
     
     num_of_reps = 10
     for i in range(num_of_reps):
