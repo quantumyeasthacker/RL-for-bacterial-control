3dea65a9e6c1a37a88e8c0314b2971a0fbc48be8
Zihang <wenzihang0506@gmail.com>
2025-04-19 20:32:44 -0500

    Update project name

--- RL_agent/run_w_wandb_single_generalized.py
+++ RL_agent/run_w_wandb_single_generalized.py
@@ -30,7 +30,7 @@ if MAIN:
                     "rep": rep}
     
     wandb.login(key = "a566d3654abf3ddf6060c24afc0e67fb4dd30c7a")
-    wandb.init(project="antibioticRL-zihang-generalized-agent-delay-30",
+    wandb.init(project="antibioticRL-zihang-generalized-agent-Apr19",
                dir=folder_name,
                name=str(trial_name),
                config=wandb_config,
